1f74197fe04da1d650b0a5592424491113727193
Carlos Garita Campos <138426063+cgaritac@users.noreply.github.com>
2024-05-31 01:16:17 -0600

    Messages

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 black = (0,0,0)
 white = (255,255,255)
@@ -13,13 +14,40 @@ pygame.display.set_caption("Helicopter")
 clock = pygame.time.Clock()
 
 img = pygame.image.load("Docs/Helicopter.png")
-x = 150
-y = 200
 
-y_move = 0
+def replay_or_quit():
+    for event in pygame.event.get([pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT]):
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif event.type == pygame.KEYDOWN:
+            continue
+        return event.key
+    return None
+
+def makeTextObjs(text, font):
+    textSurface = font.render(text, True, white)
+    return textSurface, textSurface.get_rect()
 
 def msgSurface(text):
+    smallText = pygame.font.Font("freesansbold.ttf", 20)
+    largeText = pygame.font.Font("freesansbold.ttf", 150)
+
+    titleTextSuf, titleTextRect = makeTextObjs(text, largeText)
+    titleTextRect.center = surfaceWidth/2, surfaceHeight/2
+    surface.blit(titleTextSuf, titleTextRect)
+
+    typTextSuf, typTextRect = makeTextObjs("Press any key to continue", smallText)
+    typTextRect.center = surfaceWidth/2, ((surfaceHeight/2) + 100)
+    surface.blit(typTextSuf, typTextRect)
+
+    pygame.display.update()
+    time.sleep(1)
+
+    while replay_or_quit() == None:
+        clock.tick()
     
+    main()
 
 def gameOver():
     msgSurface("Kaboom!")
@@ -27,31 +55,37 @@ def gameOver():
 def helicopter(x, y, image):
     surface.blit(img, (x,y))
 
-game_over = False
+def main():
+    x = 150
+    y = 200
+    y_move = 0
 
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
+    game_over = False
+
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    y_move = -5
+            
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    y_move = 5
+
+        y += y_move
         
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                y_move = -5
-        
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                y_move = 5
+        surface.fill(black)
+        helicopter(x,y,img)
 
-    y += y_move
-    
-    surface.fill(black)
-    helicopter(x,y,img)
+        if y > surfaceHeight-40 or y < 0:
+            gameOver()
 
-    if y > surfaceHeight-40 or y < 0:
-        gameOver()
+        pygame.display.update()
+        clock.tick(60)
 
-    pygame.display.update()
-    clock.tick(60)
-
+main()
 pygame.quit()
 quit()
