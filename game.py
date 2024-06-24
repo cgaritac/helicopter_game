188194031f2e21471c355c546bc10ecc862d7ae7
Carlos Garita Campos <138426063+cgaritac@users.noreply.github.com>
2024-06-24 13:08:12 -0600

    Colors and levels

--- game.py
+++ game.py
@@ -1,9 +1,19 @@
-from random import randint
+from random import randint, randrange
 import pygame
 import time
 
 black = (0,0,0)
 white = (255,255,255)
+
+sunset = (253,72,47)
+
+greenyellow = (184,255,0)
+brightblue = (47,228,253)
+orange = (255,113,0)
+yellow = (255,236,0)
+purple = (252,67,255)
+
+colorChoices = [greenyellow,brightblue,orange,yellow,purple]
 
 pygame.init()
 
@@ -19,9 +29,14 @@ clock = pygame.time.Clock()
 
 img = pygame.image.load("Docs/Helicopter.png")
 
-def blocks(x_block, y_block, block_width, block_height, gap):
-    pygame.draw.rect(surface, white, [x_block, y_block, block_width, block_height])
-    pygame.draw.rect(surface, white, [x_block, y_block+block_height+gap, block_width, surfaceHeight])
+def score(count):
+    font = pygame.font.Font("freesansbold.ttf", 20)
+    text = font.render("Score: " + str(count), True, white)
+    surface.blit(text, [0,0])
+
+def blocks(x_block, y_block, block_width, block_height, gap, colorChoice):
+    pygame.draw.rect(surface, colorChoice, [x_block, y_block, block_width, block_height])
+    pygame.draw.rect(surface, colorChoice, [x_block, y_block+block_height+gap, block_width, surfaceHeight])
 
 def replay_or_quit():
     for event in pygame.event.get([pygame.KEYDOWN, pygame.KEYUP, pygame.QUIT]):
@@ -34,7 +49,7 @@ def replay_or_quit():
     return None
 
 def makeTextObjs(text, font):
-    textSurface = font.render(text, True, white)
+    textSurface = font.render(text, True, sunset)
     return textSurface, textSurface.get_rect()
 
 def msgSurface(text):
@@ -76,6 +91,10 @@ def main():
     gap = imageHeight * 3
     block_move = 4
 
+    current_score = 0
+
+    blockColor = colorChoices[randrange(0,len(colorChoices))]
+
     game_over = False
 
     while not game_over:
@@ -96,7 +115,8 @@ def main():
         surface.fill(black)
         helicopter(x,y,img)
 
-        blocks(x_block, y_block, block_width, block_height, gap)
+        blocks(x_block, y_block, block_width, block_height, gap, blockColor)
+        score(current_score)
         x_block -= block_move
 
         if y > surfaceHeight-40 or y < 0:
@@ -105,23 +125,40 @@ def main():
         if x_block < (-1*block_width):
             x_block = surfaceWidth
             block_height = randint(0,int(surfaceHeight/2))
+            blockColor = colorChoices[randrange(0,len(colorChoices))]
+            current_score += 1
 
         if x+imageWidth > x_block:
             if x < x_block + block_width:
-                print("Possibly within the boundaries of x")
+                #print("Possibly within the boundaries of x")
                 if y < block_height:
-                    print("y crossover UPPER!")
+                    #print("y crossover UPPER!")
                     if x-imageWidth < block_width + x_block:
-                        print("game over hit upper")
+                        #print("game over hit upper")
                         gameOver()
         
         if x+imageWidth > x_block:
-            print("x crossover")
+            #print("x crossover")
             if y+imageHeight > block_height+gap:
-                print("y crossover lower")                
+                #print("y crossover lower")                
                 if x < block_width+x_block:
-                    print("gameover lower")
+                    #print("gameover lower")
                     gameOver()
+
+        # if x_block < (x-block_width) < x_block + block_move:
+        #     current_score += 1
+
+        if 3 <= current_score < 5:
+            block_move = 5
+            gap = imageHeight * 2.9
+
+        if 5 <= current_score < 8:
+            block_move = 6
+            gap = imageHeight * 2.8
+
+        if 8 <= current_score < 14:
+            block_move = 7
+            gap = imageHeight * 2.7
 
         pygame.display.update()
         clock.tick(60)
